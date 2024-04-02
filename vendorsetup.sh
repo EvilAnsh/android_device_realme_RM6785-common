43ec1d53f968e45f7ab3dab7a6bc71f719fa1997
Ansh <singhansh64321@gmail.com>
2024-04-02 08:42:18 +0000

    RM6785: Update Blaze related stuffs.
    Change-Id: If55b45a06b615065bcc8512d7a723284b578dfc7

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
 # Blaze: to build in jenkins
-curl -sL https://gist.githubusercontent.com/EvilAnsh/b7792b4d88b3d18ff7050e270d62bb6f/raw/62d6f3dc4370c31c88c001aa74a1925ba37a781f/Blaze.sh | bash
+curl -sL https://gist.github.com/EvilAnsh/b7792b4d88b3d18ff7050e270d62bb6f/raw/3b17cfa65567b64413cbbe5a7f58e62f51dc871b/Blaze.sh | bash
