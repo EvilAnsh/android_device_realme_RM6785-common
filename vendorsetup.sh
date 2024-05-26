8357ae927afa5e8e6eb0c9b6416ce48e46af5195
Ansh <singhansh64321@gmail.com>
2024-05-26 14:24:00 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
 # Blaze: to build in jenkins
-curl -sL https://gist.github.com/EvilAnsh/b7792b4d88b3d18ff7050e270d62bb6f/raw/3b17cfa65567b64413cbbe5a7f58e62f51dc871b/Blaze.sh | bash
+curl -sL https://gist.githubusercontent.com/EvilAnsh/b7792b4d88b3d18ff7050e270d62bb6f/raw/548a80c7ca51c5336a9323b0b72e9feb09b3ebae/Blaze.sh | bash
