c4ad10f11113756d85092c83b8ec2c99b93bfee5
SamarV-121 <samar@samarv121.dev>
2024-06-16 09:51:43 +0530

    RM6785-common: Add device FCM for stock HALs
    Change-Id: Ie04a99796737faef8159bfd680cf273020715989

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -59,7 +59,8 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    vendor/aosp/config/device_framework_matrix.xml
+    vendor/aosp/config/device_framework_matrix.xml \
+    $(COMMON_PATH)/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
 # Init
