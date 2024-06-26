ff015255877853b0854b960a0cf20ef5fa8e8001
Ansh <singhansh64321@gmail.com>
2024-06-26 06:42:43 +0000

    RM6785: Update vendorsetup.sh
    Change-Id: I5ce2df2e8434e62591b85a3818032aa347af8459

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
 # Blaze: to build in jenkins
-curl -sL https://gist.githubusercontent.com/EvilAnsh/b7792b4d88b3d18ff7050e270d62bb6f/raw/548a80c7ca51c5336a9323b0b72e9feb09b3ebae/Blaze.sh | bash
+curl -sL https://gist.github.com/EvilAnsh/b7792b4d88b3d18ff7050e270d62bb6f/raw/a976d234280608260475118ba5441b38abd3be05/Blaze.sh | bash
