0ea6e86b46f93c51bf61ce499f2a5af6f7be3de6
Mashopy <eliasgheeraert@gmail.com>
2025-08-14 21:38:47 +0200

    RM6785-common: Switch to AIDL bluetooth service
    Change-Id: I40550f0f2033e35ea609401f38f7646cb79abe14

--- RM6785.mk
+++ RM6785.mk
@@ -44,7 +44,7 @@ $(call inherit-product, hardware/dolby/dolby.mk)
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0.vendor:64
+    android.hardware.bluetooth-service.mediatek
 
 # ConfigStore
 PRODUCT_PACKAGES += \
